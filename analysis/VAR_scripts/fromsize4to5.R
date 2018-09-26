faed1577e3b173398e91ede1293c5943105435d1
ricardomayerb <ricardomayerb@gmail.com>
2018-09-26 15:17:06 -0400

    frequency variable sel

--- analysis/VAR_scripts/fromsize4to5.R
+++ analysis/VAR_scripts/fromsize4to5.R
@@ -8,19 +8,19 @@ s2_12345 <- readRDS(file = "analysis/VAR_output/Peru_s2_12345_t2.rds")
 s3_ic <- readRDS(file = "analysis/VAR_output/Peru_s3_aic_fpe_hq_sc_t2.rds")
 s3_12345 <- readRDS(file = "analysis/VAR_output/Peru_s3_12345_t2.rds")
 
-# s4_ic <- readRDS(file = "analysis/VAR_output/Peru_s4_aic_fpe_hq_sc_t2.rds")
-# s4_12345 <- readRDS(file = "analysis/VAR_output/Peru_s4_12345_t2.rds")
-# s4_6 <- readRDS(file = "analysis/VAR_output/Peru_s4_6_t2.rds")
+s4_ic <- readRDS(file = "analysis/VAR_output/Peru_s4_aic_fpe_hq_sc_t2.rds")
+s4_12345 <- readRDS(file = "analysis/VAR_output/Peru_s4_12345_t2.rds")
+s4_6 <- readRDS(file = "analysis/VAR_output/Peru_s4_6_t2.rds")
 
-# all_models <- as_tibble(rbind(s2_12345, s3_12345, s3_ic, s4_12345, s4_6, s4_ic)) %>% 
+all_models <- as_tibble(rbind(s2_12345, s3_12345, s3_ic, s4_12345, s4_6, s4_ic)) %>%
+  dplyr::select(vars_select(names(.), -starts_with("wn"))) %>%
+  dplyr::select(vars_select(names(.), -starts_with("rank"))) %>%
+  mutate(lags = unlist(lags))
+
+# all_models <- as_tibble(rbind(s2_12345, s3_12345, s3_ic)) %>% 
 #   dplyr::select(vars_select(names(.), -starts_with("wn"))) %>%
 #   dplyr::select(vars_select(names(.), -starts_with("rank"))) %>% 
 #   mutate(lags = unlist(lags)) 
-
-all_models <- as_tibble(rbind(s2_12345, s3_12345, s3_ic)) %>% 
-  dplyr::select(vars_select(names(.), -starts_with("wn"))) %>%
-  dplyr::select(vars_select(names(.), -starts_with("rank"))) %>% 
-  mutate(lags = unlist(lags)) 
 
 
 all_models <- all_models %>% 
@@ -31,6 +31,8 @@ all_models <- all_models %>%
          )
 
 all_models <- all_models %>% dplyr::distinct(short_name, .keep_all = TRUE)
+
+format(object.size(all_models), units = "auto")
 
 add_rmse_rankings <- function(tbl_with_rmses) {
   rmse_names <- names(tbl_with_rmses)
@@ -60,66 +62,72 @@ all_models_ranked  <- all_models_ranked %>%
   dplyr::select(everything(), -one_of(c("variables", "full_sample_varest")),
                 one_of(c("variables", "full_sample_varest"))) 
 
-max20_models_ranked <- all_models_ranked %>% 
-  filter_at( vars(starts_with("rank")), any_vars(. <= 20)) 
+format(object.size(all_models_ranked), units = "auto")
 
-max10_models_ranked <- all_models_ranked %>% 
-  filter_at( vars(starts_with("rank")), any_vars(. <= 10)) 
-
-
-all_models_ranked <- all_models_ranked %>% 
-  arrange(rank_1, rank_2, rank_3, rank_4, rank_5, rank_6, rank_7)
-
-lags_all <- unlist(all_models_ranked$lags)
-table(lags_all)
-
-max10_models_ranked_long  <- max10_models_ranked %>% 
+all_models_ranked_long  <- all_models_ranked %>% 
+  dplyr::select(-full_sample_varest) %>% 
   gather(key = "rmse_h", value = "rmse", 
          c("rmse_1", "rmse_2", "rmse_3", "rmse_4", "rmse_5", "rmse_6", "rmse_7")
-         ) %>% 
+  ) %>% 
   dplyr::select(vars_select(names(.), -starts_with("rank"))) %>% 
   group_by(rmse_h) %>% 
   arrange(rmse_h, rmse) %>% 
   mutate(rank_h = rank(rmse)) %>% 
   ungroup()
+
+rm(all_models)
+rm(all_models_ranked)
+
+format(object.size(all_models_ranked_long), units = "auto")
+
+
+variable_freq_by_n <- function(tbl_of_models, h_max = 8, max_rank = 10, n_freq = 4) {
   
+  vec_of_rmse_h <- sort(unique(tbl_of_models$rmse_h))
+  print(vec_of_rmse_h)
 
-variable_n_tbl <- max10_models_ranked %>% 
-  dplyr::select(variables, rmse_h, rank_h, short_name) %>% 
-  group_by(rmse_h) %>% 
-  summarise(unique_variables = list(unique(unlist(variables))),
-            non_unique_variables = list(unlist(variables)),
-            n = length(unlist(unique_variables)) - 1) %>% 
-  select(horizon, n, unique_variables, non_unique_variables)
-
-
-variable_n_tbl <- this_models_tbl %>% 
-  dplyr::select(variables, rmse_h, rank_h, long_name, horizon) %>% 
-  group_by(horizon) %>% 
-  summarise(unique_variables = list(unique(unlist(variables))),
-            non_unique_variables = list(unlist(variables)),
-            n = length(unlist(unique_variables)) - 1) %>% 
-  select(horizon, n, unique_variables, non_unique_variables)
-
-all_variables <- unlist(this_models_tbl$variables)
-
-all_variables_freq_table <- tibble::as.tibble(table(all_variables)) %>% 
-  arrange(desc(n))
-
-all_variables_h_freqs <- this_models_tbl %>% 
-  dplyr::select(variables, rmse_h, rank_h, long_name, horizon) %>% 
-  group_by(horizon) %>% 
-  summarise(freq = list(tibble::as.tibble(table(unlist(variables)))) ) 
-
-
-tbl_with_freqs_per_h <- reduce(all_variables_h_freqs$freq,
-                               full_join, by = "Var1") 
-
-names(tbl_with_freqs_per_h) <- c("variable", paste0("n_", 1:h_max))
-
-tbl_with_freqs_per_h <- tbl_with_freqs_per_h %>% 
-  mutate(ave = rowSums(.[2:(h_max+1)], na.rm = TRUE)/h_max) %>% 
-  arrange(desc(ave), desc(n_1), desc(n_2), desc(n_3), desc(n_4) ) 
+  list_best <- map(vec_of_rmse_h, 
+                   ~ tbl_of_models %>% 
+                     filter(rmse_h == .x, rank_h < max_rank +1 ) %>% 
+                     dplyr::select("variables") %>% 
+                     unlist() %>% 
+                     table() %>% 
+                     as_tibble() %>% 
+                     arrange(desc(n)) %>% 
+                     rename(., vbl = .)
+  ) 
+  
+  tbl_best <- reduce(list_best, left_join, by = c("vbl"))
+  names(tbl_best) <- c("vbl", paste("h", seq(h_max), sep = "_"))
+  
+  tbl_best <- tbl_best %>% 
+    mutate(total_n = rowSums(.[2:(h_max+1)], na.rm = TRUE))
+  
+  by_h1_20 <- tbl_best %>% 
+    arrange(desc(total_n)) %>% 
+    dplyr::select(vbl) %>% 
+    dplyr::filter(row_number() <= n_freq)
+  
+  by_total_20 <- tbl_best %>% 
+    arrange(desc(h_1)) %>% 
+    dplyr::select(vbl) %>% 
+    dplyr::filter(row_number() <= n_freq)
+  
+  both <- unique(c(by_h1_20$vbl, by_total_20$vbl))
+  
+  return( list(freqs_by_h = tbl_best, top_h1_total = both))
+}
 
 
+foo <- variable_freq_by_n(all_models_ranked_long, h_max = 7, max_rank = 20, n_freq = 4)
+# foo
 
+foo10 <- variable_freq_by_n(all_models_ranked_long, h_max = 7, max_rank = 10, n_freq = 4)
+# foo10
+
+foo5 <- variable_freq_by_n(all_models_ranked_long, h_max = 7, max_rank = 5, n_freq = 4)
+# foo5
+
+foo$top_h1_total
+foo10$top_h1_total
+foo5$top_h1_total
