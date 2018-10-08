2b7a99e42e8dc17abf3f4bcf4ee21c85e44ad591
ricardomayerb <ricardomayerb@gmail.com>
2018-10-08 17:03:43 -0400

    foo office

--- R/utils.R
+++ R/utils.R
@@ -532,3 +532,87 @@ transform_cv <- function(list_series, series_name, current_form,
 
 
 
+transform_cv_new <- function(list_series, series_name, current_form,
+                             auxiliary_ts) {
+  
+  # print("in transform_cv_new")
+  # current_form <- current_form
+  # print("in transform_cv, current form")
+  # print(current_form)
+  # print("auxiliary_ts")
+  # print(auxiliary_ts)
+  # print("list_series")
+  # print(list_series)
+  
+  
+  series_name <- series_name
+  new_series_list <- list_along(1:number_of_cv)
+  
+  if (current_form == "diff_yoy") {
+    len_initial_cond <- 1
+    
+    for (td in seq_along(1:number_of_cv)) {
+      
+      this_test_data <- list_series[[td]]
+      test_time <- time(this_test_data)
+      start_test <- min(test_time)
+      end_initial_cond <- start_test - 0.25
+      start_initial_cond <- start_test - 0.25*len_initial_cond
+      end_initial_cond_y_q <- c(year(as.yearqtr(end_initial_cond)),
+                                quarter(as.yearqtr(end_initial_cond))
+      )
+      start_initial_cond_y_q <- c(year(as.yearqtr(start_initial_cond)),
+                                  quarter(as.yearqtr(start_initial_cond))
+      )
+      initial_cond_ts <- window(auxiliary_ts, start = start_initial_cond_y_q,
+                                end = end_initial_cond_y_q)
+      
+      new_test_data <- un_diff_ts(initial_cond_ts, this_test_data)
+      
+      
+      new_series_list[[td]] <- new_test_data
+      
+    }
+    
+  }
+  
+  if (current_form == "diff") {
+    len_initial_cond <- 1
+    
+    for (td in seq_along(1:number_of_cv)) {
+      
+      this_test_data <- list_series[[td]]
+      test_time <- time(this_test_data)
+      start_test <- min(test_time)
+      end_initial_cond <- start_test - 0.25
+      start_initial_cond <- start_test - 0.25*len_initial_cond
+      end_initial_cond_y_q <- c(year(as.yearqtr(end_initial_cond)),
+                                quarter(as.yearqtr(end_initial_cond))
+      )
+      start_initial_cond_y_q <- c(year(as.yearqtr(start_initial_cond)),
+                                  quarter(as.yearqtr(start_initial_cond))
+      )
+      initial_cond_ts <- window(auxiliary_ts, start = start_initial_cond_y_q,
+                                end = end_initial_cond_y_q)
+      level_test_data <- un_diff_ts(initial_cond_ts, this_test_data)
+      pre_test_level_data <- window(auxiliary_ts, end = end_initial_cond_y_q)
+      data_and_test_level <- ts(c(pre_test_level_data, level_test_data),
+                                frequency = 4, start = start(auxiliary_ts))
+      
+      data_and_test_yoy <- make_yoy_ts(data_and_test_level, freq = 4, 
+                                       is_log = FALSE)
+      
+      new_test_data <- window(data_and_test_yoy, start = start(this_test_data),
+                              end = end(this_test_data))
+      
+      new_series_list[[td]] <- new_test_data
+    }
+    
+  }
+  
+  
+  return(new_series_list)
+  
+}
+
+
