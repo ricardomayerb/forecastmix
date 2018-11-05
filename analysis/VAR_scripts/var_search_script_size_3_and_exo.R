9f78577fdb165c566bb14c2797a37be0bb1e207e
Ricardo Mayer <ricardomayerb@gmail.com>
2018-11-05 20:34:20 -0300

    merge foo

--- analysis/VAR_scripts/var_search_script_size_3_and_exo.R
+++ analysis/VAR_scripts/var_search_script_size_3_and_exo.R
@@ -10,16 +10,16 @@ fc_horizon <- 8
 add_aic_bic_hq_fpe_lags <- FALSE # default value, can be omitted for shorter code
 vec_lags <-  c(5) # default value, can be omitted for shorter code
 
-default_vec_lag <-  c(3:4)
+default_vec_lag <-  c(2:5)
 default_treshold <- 1.65
 
-# Search_step_1 <- list(size = 2, vbl_selection_type = "none", lags = default_vec_lag)
+Search_step_1 <- list(size = 2, vbl_selection_type = "none", lags = default_vec_lag)
 Search_step_2 <- list(size = 3, vbl_selection_type = "none", lags = default_vec_lag)
 # Search_step_3 <- list(size = 4, vbl_selection_type = "incremental_auto_prechosen", n_new_prechosen = 2, lags = default_vec_lag)
 Search_step_3 <- list(size = 4, vbl_selection_type = "incremental_auto_prechosen", n_new_prechosen = 1, lags = default_vec_lag)
-# Search_step_4 <- list(size = 5, vbl_selection_type = "incremental_auto_prechosen", n_new_prechosen = 2, lags = default_vec_lag)
-# search_plan <- list(Search_step_1, Search_step_2, Search_step_3, Search_step_4)
-search_plan <- list(Search_step_2)
+Search_step_4 <- list(size = 5, vbl_selection_type = "incremental_auto_prechosen", n_new_prechosen = 2, lags = default_vec_lag)
+search_plan <- list(Search_step_1, Search_step_2, Search_step_3, Search_step_4)
+# search_plan <- list(Search_step_2)
 
 number_of_cv <- 8 # default value, can be omitted for shorter code
 train_span <- "common_max"
